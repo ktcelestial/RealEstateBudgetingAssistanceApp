a5a31c7ee3a7c0b53c4f82d1710a63ca6de300a2
ktvei <ktveiapostrophe@gmail.com>
2025-05-17 15:00:52 -0400

    general sync across devices

---
+++ backend-java-prea/src/main/java/com/example/bjprea/backend_java_prea/entity/UserAttributes.java
@@ -0,0 +1,19 @@
+package com.example.bjprea.backend_java_prea.entity;
+
+/*
+    - Utilizing Lombok library
+ */
+
+
+public class UserAttributes {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private String DOB;
+
+
+
+}
